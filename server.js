9d695be921a0c102441b8d0dea0f7a85ea5114a0
diego palacios <rafael.r191211@gmail.com>
2022-02-23 16:16:13 -0400

    depuracion

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-const http = require('http');
-
-function reponderPeticion(request,response){
-    response.end ('Hola Mundo');
-
-}
-
-let server = http.createServer(reponderPeticion);
-
-server.listen(3000);
+// depuracion masiva
